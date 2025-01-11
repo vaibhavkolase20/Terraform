3ae6d254ac5d3c4ce1fffb611c080f5603a6e076
vaibhavkolase20 <vaibhavkolase20@gmail.com>
2025-01-11 17:34:02 +0530

    Create installdocker.tf

---
+++ installdocker.tf
@@ -0,0 +1,28 @@
+# how to create docker container and inthis container install nginx emage
+# 1) install docker and after follow step
+# if you don't clone repogetory on git hub then you create one directory and create one file in this directory
+
+terraform {
+        required_providers {
+        docker = {
+        source = "kreuzwerker/docker"
+        version = "~> 2.21.0"
+}
+}
+}
+
+provider "docker" {}
+
+resource "docker_image" "nginx" {
+ name         = "nginx:latest"
+ keep_locally = false
+}
+
+resource "docker_container" "nginx" {
+ image = docker_image.nginx.latest
+ name = "nginx-tf"
+ ports {
+        internal = 80
+        external = 80
+}
+}
