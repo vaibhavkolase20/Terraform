59a2ff77f218af78593c45fdce3e72f982b9df93
vaibhavkolase20 <vaibhavkolase20@gmail.com>
2025-02-03 18:52:00 +0530

    Update create_file.tf

--- create_file.tf
+++ create_file.tf
@@ -2,3 +2,15 @@ resource "local_file" "devops" {
         filename = "/root/demo/terraform.tf"
         content = " this is my first file in terraform"
 }
+=========================================================
+# over file create first after create this file
+
+resource "random_string" "rand-str" {
+length = 16
+special = true
+override_special = "!#$&*()-_=+[]<>:?"
+}
+
+output "rand-str" {
+value = random_string.rand-str[*].result
+}
