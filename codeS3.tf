cb1edebd90d4205812cf04c87f713d0ddc79cf24
Kolase vaibhav <vaibhavkolase20@gmail.com>
2025-03-20 15:51:56 +0530

    Update codeS3.tf

--- codeS3.tf
+++ codeS3.tf
@@ -43,3 +43,42 @@ resource "aws_instance" "myserver" {
 
 # this is a url of documentation how to build  bucket
 https://chatgpt.com/share/67c42484-51c4-800f-9b12-d0584bf40434
+
+
+
+===============================================================================================================
+this is step 3-:
+
+# create one derectory like terraform
+create 3 file inthis derectory like 1) s3.tf 2) demo.tf 3) provider.tf
+# 1) add this code in s3.tf
+# this is a s3 bucket
+
+
+resource aws_s3_bucket my_bucket {
+
+        bucket = "lokalhocal"
+
+2) add this code in demo.tf
+provider "aws" {
+        region = "us-east-1"
+}
+
+3) add this code in provider.tf 
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.91.0"
+    }
+  }
+}
+
+# create aws cli using command 
+1) curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+2) sudo apt install unzip -y 
+3) unzip awscliv2.zip
+4) sudo ./aws/install
+5) aws --version
+# now apply terraform commands
+
